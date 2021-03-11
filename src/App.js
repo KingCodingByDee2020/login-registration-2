17f16283cdd95e5d974880322ceca9266aaddcd6
Manav Misra <manavm@visionwebsoft.com>
2021-03-11 17:01:16 -0600

    :construction: Render form successfully

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Box, ChakraProvider, Grid, theme } from '@chakra-ui/react';
-import React from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import './index.css';
+import { Form } from 'components';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
+          <Form />
         </Grid>
       </Box>
     </ChakraProvider>
---
+++ src/components/LoginRegistrationForm/index.js
@@ -0,0 +1,5 @@
+function LoginRegistrationForm() {
+  return <form></form>;
+}
+
+export default LoginRegistrationForm;

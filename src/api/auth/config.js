e4bd311f85351c9375c21adddbc4c47acf0812fb
Manav Misra <manavm@visionwebsoft.com>
2021-03-11 18:16:00 -0600

    :construction: api with Firebase auth

---
+++ src/api/auth/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyCaig-ZGcuwSAdVu6xJpBGk9pU0hDHBOJk',
+  authDomain: 'claim-auth.firebaseapp.com',
+  projectId: 'claim-auth',
+  storageBucket: 'claim-auth.appspot.com',
+  messagingSenderId: '495708807948',
+  appId: '1:495708807948:web:7c48e24bfba60c9817df29',
+};
+
+export default config;
